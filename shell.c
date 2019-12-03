7f80607fec068e2a5a83be86f7bfd59163e2f502
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-12-03 11:49:02 -0500

    added code to parse input and print array

---
+++ shell.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+char ** getInput() {
+  char s[100];
+  fgets(s, 100, stdin);
+
+  char * args[50]; //array to store tokens
+  char * token; //will store start of token
+  char * line = s; //the complete line stored as a pointer
+  int i=0;//index of args we are up to
+
+  while (line != NULL) {
+    token = strsep(&line, " ");
+    args[i] = token;
+    //printf("%d: [%s]\n",i,token);
+    i++;
+  }
+  //remove escape key from last index
+  args[i-1] = strsep(&(args[i-1]), "\n");
+  args[i] = NULL;
+
+  return args;
+}
+
+void printArray(char **) {
+  printf("[");
+  int c = 0;
+  for (c=0;c<i;c++) {
+    printf("'%s', ", args[c]);
+  }
+  printf("]\n");
+}
