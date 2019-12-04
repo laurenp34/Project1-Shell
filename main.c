2727f2af3139d75e952f8ed510702bb686d95180
angeliquecd <acharles-davis00@stuy.edu>
2019-12-04 11:12:40 -0500

    fixed some errors

--- main.c
+++ main.c
@@ -8,17 +8,19 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include "shell.c"
 int main(){
   printf("$");
-  int status;
-  char **args =getInput();
+  int status,f;
+  char **args;
+  getInput(args);
   fork();
   if (f){
     wait(status);
   }
   else{
-    if (strcmp(r,"exit")==0) exit();
-    execvp(args);
+  //  if (strcmp(r,"exit")==0) exit();
+    execvp(args[0],args);
   }
   return 0;
 }
