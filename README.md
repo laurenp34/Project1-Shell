d8ec35b2f493f4030002c67104c80634fd1b5bb4
angeliquecd <acharles-davis00@stuy.edu>
2019-12-09 11:43:03 -0500

    pulling

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Project2-Shell
 Lauren and Angelique, Systems period 5
+Our shell project is a program that creates its own command line and acts as a shell. It implements execvp to run typical command line functions, cd, exit and allows the user to execute multiple commands separated by a semicolon at once.
--- main.c
+++ main.c
@@ -10,39 +10,6 @@
 #include <sys/wait.h>
 #include <dirent.h>
 #include <sys/wait.h>
-<<<<<<< HEAD
-#include "shell.c"
-int main(){
-  int status;
-  int f;
-  int f2;
-  int i;
-  char * args[50][50];
-  printf("$");
-  getInput(args);
-  while (strcmp(args[0][0],"exit")!=0){
-    f = fork();
-    if (f){
-      wait(&status);
-        printf("$");
-          getInput(args);
-    }
-    else{
-    //  if (strcmp(r,"exit")==0) exit();
-      //runCommands(args);
-      i=0;
-      while(args[i][0] != NULL) {
-        f2 = fork();
-        if (f2) {
-          wait(&status);
-        }
-        else {
-          execvp(args[i][0], args[i]);
-        }
-      }
-    }
-  }
-=======
 #include "shell.h"
 #include "shell.c"
 int main(){
@@ -67,10 +34,9 @@ while (strcmp(args[0],"exit")!=0){
     if (strcmp(args[0],"cd")==0){
       return 2;
     }
-    errors=execvp(args[0], args);
+    execvp(args[0], args);
   }
 }
-printf("errors are: %d",errors);
->>>>>>> 56fdd1898cd59b385aae1a053d4323161fd69af5
+
   return 0;
 }
