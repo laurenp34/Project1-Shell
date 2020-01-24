a14c2fcd804b6e6fd2109f8c5f91e2d1759c20c3
Angelique <acharles-davis00@stuy.edu>
2020-01-24 09:25:34 -0500

    pulling

--- main.c
+++ main.c
@@ -11,21 +11,6 @@
 #include <dirent.h>
 #include <sys/wait.h>
 #include "shell.h"
-void cdfunc(int i, char *** args){
-  int h,status;
-  chdir(args[i][1]);
-      printf("made it here! %d",i);
-      i+=2;
-  while(args[i]) {
-    printf("made it in!");
-    h=fork();
-    i++;
-    if (h) {
-      wait(&status);//child process waits
-    }
-    else execvp(args[i-1][0], args[i-1]);//each command is executed
-  }
-}
 
 int main(){
   int status,f,errors,g,h;
@@ -42,7 +27,7 @@ int main(){
   while ((strcmp(args[i][0],"exit")!=0)){
   //  printArray2(args);
     i=0;
-      f=fork();
+    f=fork();
     if (f){
       wait(&status);
       if (status==512){
